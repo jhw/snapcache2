1da677f3ffbf3d8a39aede2eb0f43774c42dcd1d
Justin <justin.worrall@gmail.com>
2018-10-22 17:14:03 +0100

    added secs_to_expiry

--- src/snp_cache_item.erl
+++ src/snp_cache_item.erl
@@ -17,6 +17,9 @@
 	 terminate/2,
 	 code_change/3]).
 
+-import(wol_datetime, [timedelta/2,
+		       now_utc/0]).
+
 -record(state, {value, expiry}).
 
 %% API.
@@ -35,6 +38,8 @@ expiry(Pid) ->
 %% gen_server.
 
 init([Value, Expiry]) ->
+    SecsToExpiry=secs_to_expiry(Expiry),
+    erlang:start_timer(timer:seconds(SecsToExpiry), self(), stop),
     {ok, #state{value=Value,
 		expiry=Expiry}}.
 
@@ -48,6 +53,8 @@ handle_call(_Request, _From, State) ->
 handle_cast(_Msg, State) ->
     {noreply, State}.
 
+handle_info({timeout, _, stop}, State) ->
+    {stop, normal, State};
 handle_info(_Info, State) ->
     {noreply, State}.
 
@@ -56,3 +63,8 @@ terminate(_Reason, _State) ->
 
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
+
+%% internal functions
+
+secs_to_expiry(_) ->
+    undefined.
