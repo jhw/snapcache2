15909f063487e6e51cac4a19370e179693b5f3dc
Justin <justin.worrall@gmail.com>
2018-10-23 15:05:53 +0100

    added cache sup code

--- src/snp_cache_sup.erl
+++ src/snp_cache_sup.erl
@@ -2,15 +2,21 @@
 
 -behaviour(supervisor).
 
--export([start_link/0]).
+-export([start_link/1]).
 
 -export([init/1]).
 
+-import(snp_registry, [registry/2]).
+
+-import(wol_procs, [init_permanent_worker/3,
+		    init_permanent_sup/3]).
+
 -define(SHUTDOWN_POLICY, {one_for_one, 5, 5}).
 
-start_link() ->
-    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+start_link(Id) ->
+    supervisor:start_link({local, registry(?MODULE, Id)}, ?MODULE, [Id]).
 
-init([]) ->
-    Procs=[],
+init([Id]) ->
+    Procs=[init_permanent_worker(registry(snp_cache, Id), snp_cache, [Id]),
+	   init_permanent_sup(registry(snp_cache_sup, Id), snp_cache_sup, [Id])],
     {ok, {?SHUTDOWN_POLICY, Procs}}.
