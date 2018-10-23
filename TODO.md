5f3c0ffdd3423a12cd8bd17a8711ceebbb07f4f0
Justin <justin.worrall@gmail.com>
2018-10-23 14:54:27 +0100

    added cache item spawn code

--- TODO.md
+++ TODO.md
@@ -1,11 +1,13 @@
 ### short
 
+
 - complete cache api
 - complete cache_item_sup spawning code
 - demo to test cache/cahc
 
 ### done
 
+- cache item spawn code
 - spawn item sup with registry
 - github project
 - add cache_item expiry calculations
--- src/snp_cache_item_sup.erl
+++ src/snp_cache_item_sup.erl
@@ -2,16 +2,25 @@
 
 -behaviour(supervisor).
 
--export([start_link/1]).
+-export([start_link/1,
+	 spawn/3]).
 
 -export([init/1]).
 
 -import(snp_registry, [registry/2]).
 
+-import(wol_procs, [init_transient_worker/3,
+		    random_id/1]).
+
 -define(SHUTDOWN_POLICY, {one_for_one, 5, 5}).
 
 start_link(Id) ->
     supervisor:start_link({local, registry(?MODULE, Id)}, ?MODULE, []).
+
+spawn(Id, Value, Expiry) ->
+    Proc=init_transient_worker(random_id(32), snp_cache_item, [Value, Expiry]),
+    supervisor:start_child(registry(?MODULE, Id), Proc),
+    ok.
 
 init([]) ->
     Procs=[],
